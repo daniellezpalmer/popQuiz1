e39be1c3dad97a70a09efdb27cf001106645ca36
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-24 10:00:49 -0500

    WOWWWWWWW that wasrough

--- quiz.js
+++ quiz.js
@@ -1,1 +1,46 @@
 console.log('quiz 1');
+
+function begin() {
+	console.log("begin");
+}
+begin();
+
+function echo() {
+  return 'hello';
+}
+echo();
+
+function math(x,y,z) {
+  return (x+y)*z;
+}
+math(3,4,2);
+
+function toString(){
+  return ['now','is','the','time'];
+}
+toString();
+
+function callme (function(){
+  console.log("In the first function")
+}){
+  console.log("In the second function")
+}
+callme();
+
+function callme2 (function(){
+  console.log("In the first function")
+}){
+  console.log("In the second function")
+};
+
+function reverse("hello"){
+  return "olleh"
+}
+reverse();
+
+function sleep(){
+  console.log("before I sleep");
+  Date.now();
+  console.log("after I sleep");
+}
+sleep();
