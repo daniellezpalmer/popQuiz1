0ed4794498ea366a1fb63dc3f0416fcd3f56d5bf
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-25 12:24:58 -0500

    Updated functions into working code

--- quiz.js
+++ quiz.js
@@ -5,42 +5,60 @@ function begin() {
 }
 begin();
 
-function echo() {
-  return 'hello';
+function echo(value) {
+  return value;
 }
-echo();
+// console.log(echo('hello'));
+echo('hello');
 
 function math(x,y,z) {
   return (x+y)*z;
 }
+// console.log(math(3,4,2));
 math(3,4,2);
 
-function toString(){
-  return ['now','is','the','time'];
-}
-toString();
 
-function callme (function(){
-  console.log("In the first function")
-}){
-  console.log("In the second function")
+function toString(array){
+	let string = '';
+	for (let i = 0; i < array.length; i++) {
+		string += array[i] + ' ';
+	}
+  return string;
 }
-callme();
+// console.log(toString(['now','is','the','time']))
+toString(['now','is','the','time']);
 
-function callme2 (function(){
-  console.log("In the first function")
-}){
-  console.log("In the second function")
-};
 
-function reverse("hello"){
-  return "olleh"
+function callMe(callback) {
+	console.log('in first function');
+	callback();
 }
-reverse();
+callMe(function () {
+	console.log('in second function');
+});
+
+function callMe2(callback, string) {
+	console.log('in first function');
+	callback(string);
+}
+callMe2(function (string) {
+	console.log('in second function');
+	console.log(string);
+}, 'hello');
+
+function reverse(string){
+  let reverse = '';
+	for (var i = string.length-1; i >= 0; i--) {
+		reverse += string.charAt(i);
+	}
+	return reverse;
+}
+console.log(reverse('the iron yard'));
 
 function sleep(){
-  console.log("before I sleep");
-  Date.now();
-  console.log("after I sleep");
+  console.log("before sleep loop", Date.now());
+  setTimeout(function(){
+		console.log("after sleep loop", Date.now());
+	}, 1000)
 }
 sleep();
